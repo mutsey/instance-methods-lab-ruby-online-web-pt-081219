8e2a35e3097eda1f67bd6724caf4ee5f9f029407
Meagan Utsey <meagan.utsey@gmail.com>
2019-09-24 22:26:51 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,18 @@
+class Dog 
+  #class body
+  
+  #Instance Method Definition 
+  def bark 
+    puts "Woof!"
+  end
+end
+fido = Dog.new 
+fido.bark#> "Woof!"
+
+ def sit  
+    puts "The Dog is sitting"
+  end
+end
+fido = Dog.new 
+fido.sit#> "The Dog is sitting"
+end
