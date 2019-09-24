1937a1ac18333494976d3c6e103b519039ac0155
Meagan Utsey <meagan.utsey@gmail.com>
2019-09-24 22:25:38 +0000

    Automatically backed up by Learn

---
+++ lib/dog.rb
@@ -0,0 +1,18 @@
+class Dog 
+  #class body
+  
+  #Instance Method Definition 
+  def bark 
+    puts "Woof!"
+  end
+end
+fido = Dog.new 
+fido.bark#> "Woof!"
+
+ def bark 
+    puts "Woof!"
+  end
+end
+fido = Dog.new 
+fido.bark#> "Woof!"
+end
